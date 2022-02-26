9524b5e4ea7f090e2e41c30d732a99aa0849cc06
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-02-26 13:09:37 -0300

    task 7 do_show implemented and need some changes

--- console.py
+++ console.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
 import cmd
-
+import json
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def do_create(self, line):
         """creates a new instance of the parameter and saves it """
         if len(line) == 0:
             print("** class name missing **")
-        elif line != "BaseModel":
-            print("** class name missing **")
+            return
+        line = line.split()
+        if line[0] != "BaseModel":
+            print("** class doesn't exist **")
         else:
             new_instance = BaseModel()
             new_instance.save()
@@ -22,7 +25,31 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, line):
         """Prints the string representation of an instance based on
         the class anme and id"""
-        
+
+        line = line.split()
+        if len(line) == 0:
+            print("** class name missing **")
+            return
+
+        if line[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        if len(line) < 2:
+            print("** instance id missing **")
+            return
+        try:
+            with open("./file.json", "r") as fd:
+                database = json.loads(fd.read())
+
+                if f'{line[0]}.{line[1]}' in database.keys():
+                    print(database[f'{line[0]}.{line[1]}'])
+                    return
+        except Exception:
+            pass
+        finally:
+            print("** no instance found **")
+
 
     def do_EOF(self, line):
         "Exit the program"
