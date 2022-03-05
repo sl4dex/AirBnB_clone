41413b7ff1a55c1397e4bc63e4226fb809874422
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-03-05 08:57:49 -0800

    camila copada

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,11 +5,11 @@ Unittest for User class
 import unittest
 from models.user import User
 from datetime import datetime
-
+from models.engine.file_storage
 
 class TestUser(unittest.TestCase):
     """Class to test User"""
-
+    storage = FileStorage()
     def test_types(self):
         """test attribute types and class type"""
         model1 = User()
@@ -17,7 +17,10 @@ class TestUser(unittest.TestCase):
         self.assertEqual(type(model1.id), str)
         self.assertEqual(type(model1.created_at), datetime)
         self.assertEqual(type(model1.updated_at), datetime)
-
+        self.assertEquial(type(model1.email), str)
+        self.assertEquial(type(model1.password), str)
+        self.assertEquial(type(model1.first_name), str)
+        self.assertEquial(type(model1.last_name), str)
 
 if __name__ == '__main__':
     unittest.main()
