4649ed9c1aa7d396ccb6c047a0ae6373582c3a9d
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-03-05 00:47:34 -0300

    testing things

--- models/user.py
+++ models/user.py
@@ -4,9 +4,9 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """class user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     def __init__(self, *args, **kwargs):
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
         super().__init__(*args, **kwargs)
