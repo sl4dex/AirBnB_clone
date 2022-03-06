f41619e2a5115f6a386e8e4459f012480ed9944c
ignacio <ignacioacgarcia@gmail.com>
2022-03-06 15:40:36 -0300

    testing console

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,15 +2,26 @@
 """
 Unittest for FileStorage class
 """
+from io import StringIO
 import unittest
+from unittest.mock import patch
 from models.base_model import BaseModel
 from datetime import datetime
 from models import FileStorage
+import sys
+from console import HBNBCommand
 
 
-class FileStorage(unittest.TestCase):
-    """testing FileStorage"""
-    pass
+class TestConsole(unittest.TestCase):
+    """testing Console"""
+
+    def result(self, *args):
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd(args[0])
+        return f.getvalue().strip("\n")
+
+    def test_create(self):
+        self.assertEqual(self.result("all MyModel"), "** class doesn't exist **")
 
 
 if __name__ == '__main__':
