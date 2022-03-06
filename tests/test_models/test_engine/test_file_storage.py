c3b48e4ccf6128dee4f43a8d8a560d5d337d9e93
ignacio <ignacioacgarcia@gmail.com>
2022-03-06 11:22:12 -0300

    unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,6 +7,7 @@ import json
 from models.base_model import BaseModel
 from datetime import datetime
 from models.engine.file_storage import FileStorage
+from models.user import User
 
 class FileStorage(unittest.TestCase):
     """Class to test File Storage"""
@@ -15,11 +16,16 @@ class FileStorage(unittest.TestCase):
     def test_all(self):
         """test save method"""
         self.assertEqual(type(self.storage.all()), dict)
+        for value in self.storage.all().values():
+            self.assertFalse(type(value) is str)
+            self.assertFalse(type(value) is dict)
+        for key in self.storage.all().keys():
+            self.assertTrue(type(key) is str)
 
     def test_new(self):
         new_instance = BaseModel()
         self.storage.new(new_instance)
-        self.assertTrue(new_instance in self.storage.all().values())
+        self.assertTrue(self.storage.all()[f'BaseModel.{new_instance.id}'] is new_instance)
 
     def test_save(self):
         with open("file.json", "w") as fd:
@@ -48,6 +54,14 @@ class FileStorage(unittest.TestCase):
         self.storage.reload()
         self.assertTrue(f'BaseModel.{new.id}' in self.storage.all().keys())
         self.assertEqual(self.storage.all()[f'BaseModel.{new.id}'].__str__(), new.__str__())
+
+    def test_kwargs(self):
+        """test creating a BaseModel instance form a str dict"""
+        model1 = BaseModel()
+        model2 = BaseModel(**model1.to_dict())
+        self.storage.new(model2)
+        self.assertFalse(model1 is model2)
+        self.assertTrue(model2 in self.storage.all().values())
 
 
 if __name__ == '__main__':
