3a2e8898526f22a016073444ee577872cc7959a7
ignacio <ignacioacgarcia@gmail.com>
2022-03-06 16:10:36 -0300

    more tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -15,13 +15,39 @@ from console import HBNBCommand
 class TestConsole(unittest.TestCase):
     """testing Console"""
 
-    def result(self, *args):
+    storage = FileStorage()
+    model1 = BaseModel()
+
+    def cmd(self, *args):
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd(args[0])
         return f.getvalue().strip("\n")
 
     def test_create(self):
-        self.assertEqual(self.result("all MyModel"), "** class doesn't exist **")
+        self.assertEqual(self.cmd("create"), "** class name missing **")
+
+    def test_all(self):
+        self.assertEqual(self.cmd("all MyModel"), "** class doesn't exist **")
+
+    def test_show(self):
+        self.assertEqual(self.cmd("show"), "** class name missing **")
+        self.assertEqual(self.cmd("show MyModel"), "** class doesn't exist **")
+        self.assertEqual(self.cmd("show BaseModel"), "** instance id missing **")
+        self.assertEqual(self.cmd("show BaseModel 121212"), "** no instance found **")
+
+    def test_destroy(self):
+        self.assertEqual(self.cmd("destroy"), "** class name missing **")
+        self.assertEqual(self.cmd("destroy MyModel"), "** class doesn't exist **")
+        self.assertEqual(self.cmd("destroy BaseModel"), "** instance id missing **")
+        self.assertEqual(self.cmd("destroy BaseModel 121212"), "** no instance found **")
+
+    def test_update(self):
+        self.assertEqual(self.cmd("update"), "** class name missing **")
+        self.assertEqual(self.cmd("update MyModel"), "** class doesn't exist **")
+        self.assertEqual(self.cmd("update BaseModel"), "** instance id missing **")
+        self.assertEqual(self.cmd("update BaseModel 121212"), "** no instance found **")
+        self.assertEqual(self.cmd(f"update BaseModel {self.model1.id}"), "** attribute name missing **")
+        self.assertEqual(self.cmd(f"update BaseModel {self.model1.id} name"), "** value missing **")
 
 
 if __name__ == '__main__':
